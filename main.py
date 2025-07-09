4828e63a66007153d8637f851ec8523b2ab47cbf
harshitha2401meda2005 <harshithameda24@gmail.com>
2025-07-09 15:57:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import grades
 if __name__=="__main__":
-    l=list(map(int,input("Enter marks : ").split()))
+    l=[]
+    for i in range(5):
+        print("Enter mark: ")
+        n=int(input())
+        if n<100 and n>0:
+            l.append(n)
+        else:
+            print("Invalid input")
+            break
     a=sum(l)/len(l)
     grades.grades_classify(a)
